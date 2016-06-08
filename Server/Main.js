5885f9ffef4b1395aec7caf80b1a6fd6ac3c90c0
Филипп Дукштау < >
2016-06-08 22:45:01 +0300

    Add socket handle

--- Server/Main.js
+++ Server/Main.js
@@ -2,34 +2,34 @@
  * Created by pd6 on 08.06.2016.
  */
 
+function handler (req, res) {
+    var fs = require('fs');
+    fs.readFile(__dirname + '/index.html',
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+            res.writeHead(200);
+            res.end(data);
+        });
+}
 
 function SetUpServer() {
     var app = require('http').createServer(handler);
     var io = require('socket.io')(app);
-    var fs = require('fs');
 
-    app.listen(80);
+    app.listen(3000)
 
-    function handler (req, res) {
-        fs.readFile(__dirname + '/index.html',
-            function (err, data) {
-                if (err) {
-                    res.writeHead(500);
-                    return res.end('Error loading index.html');
-                }
-
-                res.writeHead(200);
-                res.end(data);
-            });
-    }
 
     io.on('connection', function (socket) {
-        socket.emit('news', { hello: 'world' });
-        socket.on('my other event', function (data) {
-            console.log(data);
+        var address = socket.request.connection.remoteAddress;
+
+        console.log('New connection from ' + address);
+        socket.on('newmsg', function (data) {
+            socket.broadcast.emit('mesg', data);
         });
     });
-
 }
 
 function Main() {
