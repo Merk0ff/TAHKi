810c37b8b95adcd525b156826851b6a3661d0019
PD6 <f.dukshtau@gmail.com>
2016-06-09 18:01:35 +0300

    Try add multy player struct

--- Server/Main.js
+++ Server/Main.js
@@ -2,39 +2,57 @@
  * Created by pd6 on 08.06.2016.
  */
 
-function handler (req, res) {
+var app;
+var io;
+
+function ConnectUser() {
+    io.on('connection', function (socket) {
+        var address = socket.request.connection.remoteAddress;
+
+        console.log('New connection from ' + address);
+        socket.on('newmsg', function (data) {
+            socket.emit('msg', data);
+        });
+        
+    });
+}
+
+function SendFile(res, path) {
     var fs = require('fs');
-    fs.readFile(__dirname + '/index.html',
+
+    fs.readFile(__dirname + path,
         function (err, data) {
             if (err) {
                 res.writeHead(500);
-                return res.end('Error loading index.html');
+                return res.end('Error loading ' + path);
             }
             res.writeHead(200);
             res.end(data);
         });
 }
 
+function handler (req, res) {
+    if (req.url == '/game'){
+        ConnectUser();
+        SendFile(res, '/game.html');
+    }
+    else if (req.url == '/room')
+        SendFile(res, '/room.html');
+    else {
+        SendFile(res, '/index.html');
+        ConnectUser();
+    }
+
+}
+
 function SetUpServer() {
-    var app = require('http').createServer(handler);
-    var io = require('socket.io')(app);
-
-    app.listen(3000)
-
-
-    io.on('connection', function (socket) {
-        var address = socket.request.connection.remoteAddress;
-
-        console.log('New connection from ' + address);
-        socket.on('newmsg', function (data) {
-            socket.broadcast.emit('mesg', data);
-        });
-    });
+    app = require('http').createServer(handler);
+    io  = require('socket.io')(app);
+    app.listen(8000);
 }
 
 function Main() {
     SetUpServer();
-    return 0;
 }
 
 if (require.main === module) {
