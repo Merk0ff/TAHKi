4c19d7c7633cac75f2b9e515bcbcaf8b49ad0523
KM6 <mitikostya@yandex.ru>
2016-06-11 12:21:34 +0400

    fixed debil

--- Server/Main.js
+++ Server/Main.js
@@ -6,7 +6,7 @@ var app;
 var io;
 var http;
 
-var Users = [], UserCounter = 0, Rooms = [], RoomCounter = 0;
+var Users = [], UserCounter = 0, Rooms = [];
 
 function getRandomArbitary(min, max) {
     return Math.round(Math.random() * (max - min) + min);
@@ -15,6 +15,7 @@ function getRandomArbitary(min, max) {
 function AddNewUser(data) {
     var send = {
         userid: data.userid,
+        roomid: data.roomid,
         userServerId: UserCounter
     };
 
@@ -22,9 +23,7 @@ function AddNewUser(data) {
         userid: data.userid,
         roomid: data.roomid
     };
-
     UserCounter++;
-
     return send;
 }
 
@@ -32,46 +31,59 @@ function ConnectUser() {
     io.on('connection', function (socket) {
         var address = socket.request.connection.remoteAddress;
 
+        //io.sockets.removeAllListeners();
+
         console.log('New connection from ' + address);
 
         // Crate new room callback
         socket.on('NewRoom', function (data) {
             var RoomId = getRandomArbitary(400, 30000);
 
-            Rooms[RoomCounter].id = RoomId;
-            Rooms[RoomCounter].users = [];
-            Rooms[RoomCounter].blteam = 0;
-            Rooms[RoomCounter].reteam = 0;
+            Rooms[RoomId] = {
+                id: RoomId,
+                users: undefined,
+                blteam: 0,
+                reteam: 0
+            };
+            Rooms[RoomId].users = [];
 
-            socket.emit('BackNewRoomId', {roomid: RoomId, userServerId: RoomCounter});
-            RoomCounter++;
+            socket.emit('BackNewRoomId', RoomId);
         });
 
         // Join room callback
         socket.on('JoinRoom', function (data) {
             var send = AddNewUser(data);
-
-            if (Rooms[data.userServerId].users.length >= 10)
+            
+            if (Rooms[data.roomid].users.length >= 10)
                 socket.emit('Err', 0);
 
             socket.join(data.roomid.toString());
-            Rooms[data.userServerId].users.push(data);
+            Rooms[data.roomid].users.push(data);
 
-            io.sockets.in(data.roomid.toString()).emit('BackNewRoomUser', send);
+            send.users = Rooms[data.roomid].users;
+
+            socket.emit('BackNewRoomUser', send);
         });
 
         // Join team callback
         socket.on('JoinTeam', function (data) {
             Users[data.userServerId].team = data.team;
 
-            if (Rooms[data.userServerId].reteam >= 5 || Rooms[data.userServerId].blteam >= 5)
+            if (Rooms[data.roomid].reteam >= 5 || Rooms[data.roomid].blteam >= 5)
                 socket.emit('Err', 1);
 
+            if (data.team)
+                Rooms[data.roomid].blteam++;
+            else
+                Rooms[data.roomid].reteam++;
+            
             io.sockets.in(data.roomid.toString()).emit('BackJoinTeam', {team: data.team, userid: data.userid});
         });
 
+        /*
         socket.on('disconnect', function () {
         });
+        */
     });
 }
 
@@ -92,7 +104,6 @@ function SendFile(res, path) {
 function Serverhandler() {
     app.get('/', function (req, res) {
         SendFile(res, '/index.html');
-        ConnectUser();
     });
 
     app.get('/game', function (req, res) {
@@ -107,6 +118,7 @@ function SetUpServer() {
     http = require('http').Server(app);
     io = require('socket.io')(http);
 
+    ConnectUser();
     Serverhandler();
     app.use(exp.static('../client'));
 
--- client/js/conection.js
+++ client/js/conection.js
@@ -1,21 +1,30 @@
+var index_red = 1;
+var index_blue = 1;
 
 function GetData() {
     socket.on('BackJoinTeam', function (backdata) {
-        data.team = backdata.team;
-        data.userid = backdata.userid;
+        if (backdata.team == 0) {
+            $("#red_" + index_red).text(backdata.userid);
+            index_red++;
+        }
+        else if (backdata.team == 1) {
+            $("#blue_" + index_blue).text(backdata.userid);
+            index_blue++;
+        }
     });
     socket.on('BackNewRoomUser', function (backdata) {
         data.userid = backdata.userid;
         data.userServerId = backdata.userServerId;
+        data.roomid = backdata.roomid;
     });
-    
+
     socket.on('BackNewRoomId', function (backdata) {
         data.roomid = backdata;
         $("#RoomIdLabel").text("Your room name");
         $("#RoomId").val(data.roomid.toString());
         $("#RoomId").attr("readonly", "readonly");
         $("#drop").slideDown("fast");
-        JoinRoom($("#RoomId").val(), data.roomid);
+        JoinRoom($("#Nick").val(), data.roomid);
     });
 }
 
@@ -26,7 +35,7 @@ function CreateRoom() {
 function JoinRoom(nick, roomid) {
     data.userid = nick;
     data.roomid = roomid;
-    
+
     socket.emit('JoinRoom', data);
     $("#teams").show("fast");
 }
