de81a276667a95330734fd589493cd2547233231
PD6 <f.dukshtau@gmail.com>
2016-06-11 11:18:57 +0300

    Add room handle

--- Server/Main.js
+++ Server/Main.js
@@ -6,7 +6,7 @@ var app;
 var io;
 var http;
 
-var Users = [], UserCounter = 0;
+var Users = [], UserCounter = 0, Rooms = [], RoomCounter = 0;
 
 function getRandomArbitary(min, max) {
     return Math.round(Math.random() * (max - min) + min);
@@ -38,22 +38,39 @@ function ConnectUser() {
         socket.on('NewRoom', function (data) {
             var RoomId = getRandomArbitary(400, 30000);
 
-            socket.emit('BackNewRoomId', RoomId);
+            Rooms[RoomCounter].id = RoomId;
+            Rooms[RoomCounter].users = [];
+            Rooms[RoomCounter].blteam = 0;
+            Rooms[RoomCounter].reteam = 0;
+
+            socket.emit('BackNewRoomId', {roomid: RoomId, userServerId: RoomCounter});
+            RoomCounter++;
         });
 
         // Join room callback
         socket.on('JoinRoom', function (data) {
             var send = AddNewUser(data);
+
+            if (Rooms[data.userServerId].users.length >= 10)
+                socket.emit('Err', 0);
+
             socket.join(data.roomid.toString());
+            Rooms[data.userServerId].users.push(data);
 
             io.sockets.in(data.roomid.toString()).emit('BackNewRoomUser', send);
         });
 
+        // Join team callback
         socket.on('JoinTeam', function (data) {
             Users[data.userServerId].team = data.team;
 
+            if (Rooms[data.userServerId].reteam >= 5 || Rooms[data.userServerId].blteam >= 5)
+                socket.emit('Err', 1);
+
             io.sockets.in(data.roomid.toString()).emit('BackJoinTeam', {team: data.team, userid: data.userid});
-            
+        });
+
+        socket.on('disconnect', function () {
         });
     });
 }
